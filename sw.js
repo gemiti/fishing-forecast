a76b0bd858b170ba175344bea52c0dc7d6bcda93
gemiti <84082756+gemiti@users.noreply.github.com>
2026-08-20 10:45:38 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fishing-v6';
+const CACHE_NAME = 'fishing-v7';
 const urlsToCache = ['./', './index.html', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
